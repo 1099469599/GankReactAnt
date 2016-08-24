486c69408bb8708790cf8b6bde976fbc983340c2
linkaipeng <linkaipeng1992@gmail.com>
2016-08-24 10:29:39 +0800

    [modify] List item add click event.

--- app/ui/list-item.js
+++ app/ui/list-item.js
@@ -3,15 +3,24 @@ import 'style/list.css';
 
 export default React.createClass({
 
+    onItemClick: function(){
+        window.open(this.props.url);
+    },
+
     render: function() {
         var item = this.props;
     	if (item != undefined) {
     		return (
-                <div className='list-item'>
-                    <div className='list-item-title'>{item.desc}</div>
-                    <div className='list-item-who'>{item.who}</div>
-                    <div className='list-item-type'>{item.type}</div>
-                    <div className='list-item-time'>{item.createdAt}</div>
+                <div className='list-item' onClick={this.onItemClick}>
+                    <div className='list-item-top'>
+                        <label className='list-item-title'>{item.desc}</label>
+                        <label className='list-item-type'>{item.type}</label>
+                    </div>
+
+                    <div className='list-item-bottom'>
+                        <label className='list-item-who'>{item.who}</label>
+                        <label className='list-item-time'>{item.createdAt}</label>
+                    </div>
                 </div>
             );
     	}
