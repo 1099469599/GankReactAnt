94016d8934c6d0ce308d0abff22d626994d47203
linkaipeng <linkaipeng1992@gmail.com>
2016-08-26 17:40:25 +0800

    [modify] Item count of per page data increase to 20.

--- app/api/data-business.js
+++ app/api/data-business.js
@@ -4,7 +4,7 @@ import store from 'store';
 export function getData(type, page){
 	return $.ajax({
         type: 'GET',
-        url: 'http://gank.io/api/data/'+type+'/10/'+page,
+        url: 'http://gank.io/api/data/'+type+'/20/'+page,
         dataType: 'json',
         success: function(respone) {
             if (!respone.error) {
