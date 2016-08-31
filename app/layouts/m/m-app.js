76f728ee84272843af7a7c2312e6165802864614
linkaipeng <linkaipeng1992@gmail.com>
2016-08-31 22:59:11 +0800

    [modify] Add css and meta for mobile.

---
+++ app/layouts/m/m-app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router';
+import Menu from 'ui/menu';
+
+import DataList from 'ui/list';
+import 'style/app.css';
+import 'antd/dist/antd.css';
+import 'style/m/common.css';
+
+
+export default React.createClass({
+    render: function() {
+        return (
+            <div>
+                mobile
+            </div>
+        )
+    }
+});
